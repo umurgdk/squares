aa77929f9d07f43052fb0f280d987562d94c0c3d
umurgdk <umurgdk@gmail.com>
2021-11-06 13:34:32 +0300

    Initial Commit

---
+++ Squares/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Squares
+//
+//  Created by Umur Gedik on 6.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Squares/SquaresApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquaresApp.swift
+//  Squares
+//
+//  Created by Umur Gedik on 6.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SquaresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
