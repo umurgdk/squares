b6c98c70959d79084a832c84f65d53470a9f4407
umurgdk <umurgdk@gmail.com>
2022-01-02 17:03:28 +0300

    Fix colorScheme to dark mode

--- Squares/Views/ContentView.swift
+++ Squares/Views/ContentView.swift
@@ -27,7 +27,7 @@ struct ContentView: View {
             }
             .padding()
             .overlay(trackPadOverlay)
-        }
+        }.preferredColorScheme(.dark)
     }
     
     @ViewBuilder
